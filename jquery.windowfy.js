ca4993375093a4300ec41f743bf959056d70a53a
Evan McIntire <mcintire.evan@gmail.com>
2015-03-06 18:11:47 -0500

    Fixed CSS import, add exitmode param

--- jquery.windowfy.js
+++ jquery.windowfy.js
@@ -1,12 +1,19 @@
 (function ($) {
-    $('head').append('<link rel="stylesheet" href="jquery.windowfy.css" type="text/css" />');
+    $('head').append('<link rel="stylesheet" href="windowfy/jquery.windowfy.css" type="text/css" />');
     $.fn.windowfy = function (params) {
-        params = this.extend({ title: 'Window', minimize: true, close: true } , params);
+        params = this.extend({
+            title: 'Window',
+            minimize: true,
+            close: true,
+            exitmode: 'destroy',
+            id: ''
+        }, params);
         return this.each(function () {
             var body = this;
 
             var holder = $('<div/>').attr({
-                class: 'windowfy'
+                class: 'windowfy',
+                id: params.id
             }).appendTo('body');
 
             var header = $('<div/>').attr({
@@ -33,7 +40,16 @@
                 $('<div/>').attr({
                     class: 'windowfy-exit'
                 }).html('x').on('click', function () {
-                    holder.remove();
+                    switch (params.exitmode) {
+                        case 'destroy': {
+                            holder.remove();
+                            break;
+                        }
+                        case 'hide': {
+                            holder.hide();
+                            break;
+                        }
+                    }
                 }).appendTo(options);
             }
 

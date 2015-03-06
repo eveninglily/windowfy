0ef1a499ab95c90489883fad8c9577b39e4fbcd1
Evan McIntire <mcintire.evan@gmail.com>
2015-03-05 21:46:23 -0500

    Changed style names to be unique to the plugin

--- jquery.windowfy.js
+++ jquery.windowfy.js
@@ -1,12 +1,12 @@
 (function ($) {
     $('head').append('<link rel="stylesheet" href="jquery.windowfy.css" type="text/css" />');
     $.fn.windowfy = function (params) {
-        params = this.extend({ title: 'window' } , params);
+        params = this.extend({ title: 'Window' } , params);
         return this.each(function () {
             var body = this;
 
             var holder = $('<div/>').attr({
-                class: 'jquery-DraggableWindow'
+                class: 'windowfy'
             }).appendTo('body');
 
             var header = $('<div/>').attr({
@@ -14,21 +14,21 @@
             });
 
             var title = $('<div/>').attr({
-                class: 'windowTitle grab'
+                class: 'windowfy-title windowfy-grab'
             }).html(params.title).appendTo(header);
 
             var options = $('<div/>').attr({
-                class: 'options'
+                class: 'windowfy-options'
             }).appendTo(header);
 
             var hide = $('<div/>').attr({
-                class: 'minimize'
+                class: 'windowfy-minimize'
             }).html('-').on('click', function () {
                 $(body).toggle();
             }).appendTo(options);
 
             var e = $('<div/>').attr({
-                class: 'exit'
+                class: 'windowfy-exit'
             }).html('x').on('click', function () {
                 holder.remove();
             }).appendTo(options);
@@ -38,18 +38,18 @@
             var offsetX = 0;
             var offsetY = 0;
 
-            $(document).on('mousedown', '.windowTitle', function (evt) {
-                $(this).removeClass('grab');
-                $(this).addClass('grabbing');
+            $(document).on('mousedown', '.windowfy-title', function (evt) {
+                $(this).removeClass('windowfy-grab');
+                $(this).addClass('windowfy-grabbing');
                 offsetX = evt.pageX - holder.offset().left;
                 offsetY = evt.pageY - holder.offset().top;
             }).on('mouseup', function () {
-                title.addClass('grab');
-                title.removeClass('grabbing');
+                title.addClass('windowfy-grab');
+                title.removeClass('windowfy-grabbing');
             }).on('mousemove', function (evt) {
                 evt.preventDefault();
                 document.getSelection().removeAllRanges();
-                if (title.hasClass('grabbing')) {
+                if (title.hasClass('windowfy-grabbing')) {
                     holder.css({ left: (evt.pageX - offsetX) + 'px' });
                     holder.css({ top: (evt.pageY - offsetY) + 'px' });
                 }

84eae41cd09ee3db633d607a95dde7e91648b194
Evan McIntire <mcintire.evan@gmail.com>
2015-03-17 09:43:52 -0400

    Slight cleanup of code

--- src/jquery.windowfy.js
+++ src/jquery.windowfy.js
@@ -1,5 +1,4 @@
 (function ($) {
-    $('head').append('<link rel="stylesheet" href="windowfy/jquery.windowfy.css" type="text/css" />');
     $.widget('windowfy.windowfy', {
         options: {
             title: 'Window',
@@ -17,27 +16,27 @@
         _create: function () {
             var _this = this;
             this.window = $('<div/>').attr({
-                class: 'windowfy',
-                id: this.options.id
+                'class': 'windowfy',
+                'id': this.options.id
             }).appendTo(this.element.parent());
 
             var body = $('<div>').attr('class','windowfy-body').append(this.element);
-this.body = body;
+            this.body = body;
             var header = $('<div/>').attr({
                 width: '100%'
             });
 
             var title = $('<div/>').attr({
-                class: 'windowfy-title windowfy-grab'
+                'class': 'windowfy-title windowfy-grab'
             }).html(this.options.title).appendTo(header);
 
             var opts = $('<div/>').attr({
-                class: 'windowfy-option'
+                'class': 'windowfy-option'
             }).appendTo(header);
-this.opts = opts;
+            this.opts = opts;
             if (this.options.minimize) {
                 $('<div/>').attr({
-                    class: 'windowfy-minimize'
+                    'class': 'windowfy-minimize'
                 }).html('-').on('click', function () {
                     _this.options.onMinimize.call(_this);
                 }).appendTo(opts);
@@ -45,7 +44,7 @@ this.opts = opts;
 
             if (this.options.close) {
                 $('<div/>').attr({
-                    class: 'windowfy-exit'
+                    'class': 'windowfy-exit'
                 }).html('x').on('click', function () {
                     _this.options.onClose.call(_this);
                 }).appendTo(opts);
@@ -73,15 +72,6 @@ this.opts = opts;
                     _this.window.css({ top: (evt.pageY - offsetY) + 'px' });
                 }
             });
-        },
-
-        addOption: function(html, callback) {
-            var _this = this;
-            $('<div/>').attr({
-                class: 'windowfy-option'
-            }).html('x').on('click', function () {
-                _this.options.onClose.call(_this);
-            }).appendTo(this.opts);
         }
     });
 })( jQuery );
